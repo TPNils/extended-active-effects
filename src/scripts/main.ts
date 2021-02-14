f1c778bae399ba61d10c6aeec5aea295ba041e00
Nils <nils.yseboodt@gmail.com>
2021-02-14 19:37:52 +0100

    feat: expose passive effects

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,14 @@
 import { ExtendActiveEffectService } from './extend-active-effects.js';
-import { registerPassiveEffects } from './passive-effect.js';
+import { PassiveEffect, registerPassiveEffects } from './passive-effect.js';
+import { StaticValues } from './static-values.js';
 
 const extendActiveEffectService = new ExtendActiveEffectService();
 extendActiveEffectService.register();
 
 Hooks.on('init', () => {
   registerPassiveEffects();
+
+  game[StaticValues.moduleName] = {
+    PassiveEffect: PassiveEffect,
+  }
 })

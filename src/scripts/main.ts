3b8045eb4b2aac13d40a6991ba324179615b6d2f
Nils <nils.yseboodt@gmail.com>
2021-02-14 18:40:27 +0100

    fix: init passive effects

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,9 @@
 import { ExtendActiveEffectService } from './extend-active-effects.js';
+import { registerPassiveEffects } from './passive-effect.js';
 
 const extendActiveEffectService = new ExtendActiveEffectService();
 extendActiveEffectService.register();
+
+Hooks.on('init', () => {
+  registerPassiveEffects();
+})
